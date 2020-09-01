e64e72c638ac9e51b9d1a290d3f005c209d2d760
AnibalUlises <67528089+anibalulises93@users.noreply.github.com>
2020-08-31 20:59:29 -0400

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,68 @@
+//console.log("Hola mundo desde Javascript");
+
+//DOM
+/*let tabla = document.querySelector(".table");
+console.log(tabla);
+
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+    console.log(link);
+})*/
+
+/*let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){
+        console.log(this)
+    })
+})*/
+
+//Obtener los elementos de la clase .close
+
+//let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+
+/*links.forEach(function(link){
+    //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+
+        return false;
+    });
+});*/
+/*
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star");
+})
+*/
+
+//Obtener los elementos de la clase .close
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+links.forEach(function(link){
+    //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitarle las clases de animación que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases para animar su salida
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        //setTimeout(() => {location.href = "/";},600);
+        setTimeout(function(){
+            location.href = "/";
+        },600);
+        return false;
+    });
+});
+
